0ab6dd226af64c867594ceb8e616a3222bc1eb79
Aegon007 <1277223029@qq.com>
2020-12-08 19:42:49 -0500

    channel choice ready

--- load_slice_IQ.py
+++ load_slice_IQ.py
@@ -18,7 +18,7 @@ def normalizeData(v):
     return v
 
 
-def read_f32_bin(filename, start_ix):
+def read_f32_bin(filename, start_ix, channel_first):
     with open(filename, 'rb') as bin_f:
         iq_seq = np.fromfile(bin_f, dtype='<f4')
         n_samples = iq_seq.shape[0] // 2
@@ -29,18 +29,22 @@ def read_f32_bin(filename, start_ix):
         IQ_data[1, :] = iq_seq[range(1, iq_seq.shape[0], 2)]
 
     del iq_seq
-    return IQ_data[:, start_ix:]
+    rtn_data = IQ_data[:, start_ix:]
+    if not channel_first:
+        rtn_data = rtn_data.T
+    return rtn_data
 
 
-def dev_bin_dataset(glob_dat_path, n_samples, start_ix=0):
+def dev_bin_dataset(glob_dat_path, n_samples, start_ix, channel_first):
     filelist = sorted(glob.glob(glob_dat_path))
-    all_IQ_data = read_f32_bin(filelist[0], start_ix)
+    all_IQ_data = read_f32_bin(filelist[0], start_ix, channel_first)
     isEnoughSample = False
 
-    if all_IQ_data.shape[1] < n_samples:
+    axis = 1 if channel_first else 0
+    if all_IQ_data.shape[axis] < n_samples:
         for f in filelist[1:]:
-            all_IQ_data = np.concatenate((all_IQ_data, read_f32_bin(f, start_ix)), axis=1)
-            if all_IQ_data.shape[1] >= n_samples:
+            all_IQ_data = np.concatenate((all_IQ_data, read_f32_bin(f, start_ix, channel_first)), axis=axis)
+            if all_IQ_data.shape[axis] >= n_samples:
                 isEnoughSample = True
                 break
     else:
@@ -50,11 +54,14 @@ def dev_bin_dataset(glob_dat_path, n_samples, start_ix=0):
         print("ERROR! There are not enough samples to satisfy dataset parameters. Aborting...")
         sys.exit(-1)
 
-    all_IQ_data = all_IQ_data[:, 0:n_samples]
+    if channel_first:
+        all_IQ_data = all_IQ_data[:, 0:n_samples]
+    else:
+        all_IQ_data = all_IQ_data[0:n_samples, :]
     return all_IQ_data
 
 
-def loadData(args):
+def loadData(args, channel_first):
     n_slices_per_dev = args.num_slice
     start_ix = args.start_ix
     file_key = args.file_key
@@ -65,20 +72,27 @@ def loadData(args):
         tmp = os.path.join(args.root_dir, n)
         dev_dir_list.append(tmp)
 
-    n_devices = len(dev_dir_list)
-    slice_dims = (2, args.slice_len)
     stride = args.stride
-    samps_to_retrieve = (n_slices_per_dev - 1) * stride + slice_dims[1]
+    n_devices = len(dev_dir_list)
+    if channel_first:
+        slice_dims = (2, args.slice_len)
+        samps_to_retrieve = (n_slices_per_dev - 1) * stride + slice_dims[1]
+    else:
+        slice_dims = (args.slice_len, 2)
+        samps_to_retrieve = (n_slices_per_dev - 1) * stride + slice_dims[0]
 
     x_train, y_train, x_test, y_test = [], [], [], []
     split_ratio = {'train': 0.8, 'val': 0.2}
     for i, d in enumerate(dev_dir_list):
-        pre_X_data = dev_bin_dataset(os.path.join(d, file_key), samps_to_retrieve, start_ix)
+        pre_X_data = dev_bin_dataset(os.path.join(d, file_key), samps_to_retrieve, start_ix, channel_first)
 
         X_data_pd = []
         count_s = 0
         for j in range(0, samps_to_retrieve, stride):
-            X_data_pd.append(pre_X_data[:, j:j+slice_dims[1]])
+            if channel_first:
+                X_data_pd.append(pre_X_data[:, j:j+slice_dims[1]])
+            else:
+                X_data_pd.append(pre_X_data[j:j+slice_dims[0], :])
             count_s += 1
             if count_s == n_slices_per_dev:
                 break
@@ -101,8 +115,12 @@ def loadData(args):
         del X_data_pd
 
     if args.D2:
-        x_train = x_train[:, np.newaxis, :, :]
-        x_test = x_test[:, np.newaxis, :, :]
+        if channel_first:
+            x_train = x_train[:, :, np.newaxis, :]
+            x_test = x_test[:, :, np.newaxis, :]
+        else:
+            x_train = x_train[:, np.newaxis, :, :]
+            x_test = x_test[:, np.newaxis, :, :]
 
     y_train = np_utils.to_categorical(y_train, n_devices)
     y_test = np_utils.to_categorical(y_test, n_devices)
@@ -131,14 +149,14 @@ def parseArgs(argv):
     parser.add_argument('-s', '--stride', type=int, default=1, help='Stride used for windowing.')
     parser.add_argument('-f', '--file_key', default='*.bin', help='used to choose different filetype, choose from *.bin/*.sigmf-meta')
     parser.add_argument('--D2', action='store_true', help='')
+    parser.add_argument('-cf', '--channel_first', action='store_true', help='if set channel first otherwise channel last')
     opts = parser.parse_args()
     return opts
 
 
 if __name__ == "__main__":
     opts = parseArgs(sys.argv)
-    x_train, y_train, x_test, y_test, NUM_CLASS = loadData(opts)
-    pdb.set_trace()
+    x_train, y_train, x_test, y_test, NUM_CLASS = loadData(opts, channel_first)
     print('train data shape: ', x_train.shape, 'train label shape: ', y_train.shape)
     print('test data shape: ', x_test.shape, 'test label shape: ', y_test.shape)
     print('all test done!')
